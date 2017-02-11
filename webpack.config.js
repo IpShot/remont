1304ba56140ec3820dceafea810ca9a0445667e6
Roman Zhernosek <ipshot007@gmail.com>
2017-02-11 16:31:48 +0300

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+var path = require('path');
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+var SOURCE_DIR = path.resolve(__dirname + '/src');
+
+module.exports = {
+  devtool: 'source-map',
+
+  entry: {
+    app: [
+      'webpack-dev-server/client?/',
+      'webpack/hot/only-dev-server',
+      'regenerator-runtime/runtime',
+      './src/app/client.js'
+    ]
+  },
+
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, '/build'),
+    filename: '[name].js'
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      sourceMap: true
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      splitBlocks: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [ SOURCE_DIR ],
+        exclude: [/node_modules/],
+        query: {
+          presets: ['es2015', 'stage-0', 'react'],
+          plugins: [
+            'transform-decorators-legacy',
+            '@mangojuice/core/babel/proxy'
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!cssnext-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    modules: [
+      'node_modules',
+      path.resolve(__dirname)
+    ],
+    // alias: {
+    //   'react': 'preact-compat/dist/preact-compat',
+    //   'react-dom': 'preact-compat/dist/preact-compat'
+    // }
+  },
+
+  devServer: {
+    hot: true,
+    publicPath: '/',
+    inline: false,
+    lazy: false,
+    quiet: true,
+    noInfo: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    stats: { colors: true },
+    historyApiFallback: {
+      disableDotRule: true
+    }
+  }
+};
