ac81866459dfb46ace5a9d5eb90ae509c2da6e16
Roman Zhernosek <ipshot007@gmail.com>
2017-02-12 22:05:27 +0300

    added css load

--- src/app/components/Hello/index.js
+++ src/app/components/Hello/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import style from './style.css';
 
 const Hello = ({ brandName }) => (
-  <div>
+  <div className={style.hello}>
     {`Привет! Я ${brandName} и я сделаю вам крутейший ремонт!`}
   </div>
 );
